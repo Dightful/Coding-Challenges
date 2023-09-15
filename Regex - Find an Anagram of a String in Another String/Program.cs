b721dcf03b1fefd4fdec16cea9005c2ee9248888
Dightful <73068028+Dightful@users.noreply.github.com>
2023-09-15 16:46:53 +0100

    Add files via upload

---
+++ Regex - Find an Anagram of a String in Another String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Regex___Find_an_Anagram_of_a_String_in_Another_String
+{
+    class Program
+    {
+
+        public static bool AnagramStrStr(string needle, string haystack)
+        {
+            if (Regex.IsMatch(haystack, needle) == false)
+            {
+                char[] charArray = needle.ToCharArray();
+                Array.Reverse(charArray);
+                needle = new string(charArray);
+            }
+            return Regex.IsMatch(haystack, needle);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AnagramStrStr("bol","sdfbolkug"));
+        }
+    }
+}
